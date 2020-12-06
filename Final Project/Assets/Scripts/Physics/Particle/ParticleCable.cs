e4a5a4dc144e2f8d7ac8bf8694bc43e0adfa89a4
GoGetAVegan <maximus.lorah@mymail.champlain.edu>
2020-12-06 15:05:17 -0500

    Initial push for Final Project

---
+++ Final Project/Assets/Scripts/Physics/Particle/ParticleCable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleCable : Particle2DLink
+{
+    float mMaxLength;
+    float mRestitution;
+
+    public void Initialize(GameObject object1, GameObject object2, float length, float restitution)
+    {
+        mObject1 = object1;
+        mObject2 = object2;
+        mMaxLength = length;
+        mRestitution = restitution;
+    }
+    public override void CreateContacts(List<Particle2DCollision> contacts)
+    {
+        float length = Vector2.Distance(mObject1.transform.position, mObject2.transform.position);
+        if (length < mMaxLength)
+        {
+            return;
+        }
+
+        Vector2 normal = mObject2.transform.position - mObject1.transform.position;
+        normal.Normalize();
+        float penetration = length - mMaxLength;
+
+        Particle2DCollision newParticleContact = gameObject.AddComponent<Particle2DCollision>();
+        newParticleContact.Initialize(mObject1, mObject2, 0.0f, penetration, normal, Vector2.zero, Vector2.zero);
+        contacts.Add(newParticleContact);
+    }
+}
