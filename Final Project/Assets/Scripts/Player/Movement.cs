5a3aedfcba516ac1a319d923a1d9d6832be63663
GoGetAVegan <maximus.lorah@mymail.champlain.edu>
2020-12-13 16:03:51 -0500

    Final scene for presentation

---
+++ Final Project/Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        gameObject.transform.position = new Vector2(transform.position.x + (h * speed),
+           transform.position.y + (v * speed));
+
+    }
+
+}
